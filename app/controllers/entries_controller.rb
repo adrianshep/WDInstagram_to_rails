15355e02e44619ac133f137cbbf00724b85e8994
Adrian Shepelavey <adrianshepelavey@gmail.com>
2015-07-27 22:39:07 -0400

    models and cleanup

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,46 @@
 class EntriesController < ApplicationController
-
+  # index
   def index
+    @entries = Entry.all.order(:id).reverse
+  end
 
+  # show
+  def show
+    @entry = Entry.find( params[:id] )
+  end
+
+# new
+  def new
+    @entry = Entry.new
+  end
+
+# create
+  def create
+    @entry = Entry.new( params[:entry] )
+    @entry.update( date_taken: Time.now.strftime("%B %d, %Y") )
+    if @entry.save
+      redirect "/entries/#{@entry.id}"
+    else
+      redirect "/posts/new"
+    end
+  end
+
+# edit
+  def edit
+    @entry = Entry.find( params[:id] )
+  end
+
+# update
+  def update
+    @entry = Entry.find( params[:id] )
+    @entry.update( params[:entry] )
+    redirect "/entries/#{@entry.id}"
+  end
+
+# delete
+  def destroy
+    @entry = Entry.find( params[:id] )
+    @entry.destroy
+  redirect "/"
   end
 end
